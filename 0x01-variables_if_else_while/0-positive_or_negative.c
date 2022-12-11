a05cfcb3ee8097913dc61f37fa96fb8d996f1300
Aphranm <aphran@gmail.com>
2022-12-11 12:10:07 -0800

    Positive anything is better than negative nothing

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,21 +2,17 @@
 #include <time.h>
 #include <stdio.h>
 
-/* made by Aphran with help of some friends about the term 
+/** made by Aphran with help of some friends about the term 
  * srand and RAND MAX
  * the above lines are the predeuned moudules that we are going
- * to use as sources in our code */
+ * to use as sources in our code*/
 
 int main(void)
 {
-/* in main is the declaration of our code 
- * and every code is written inside it. */
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* this show the randowm number - the maxmum value returned 
-	 * by the  rand function */
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n < 0)
