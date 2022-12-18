b767a5820f1945e85c8698a44d94dc8ac3c9014c
Aphranm <aphran@gmail.com>
2022-12-17 23:51:07 -0800

    file creation

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Prints "APHRAN" followed by a new line.
+ *
+ */
+int main(void)
+{
+	_putchar('A');
+	_putchar('P');
+	_putchar('P');
+	_putchar('H');
+	_putchar('R');
+	_putchar('A');
+	_putchar('N');
+	_putchar('\n');
+
+	return (0);
+}
