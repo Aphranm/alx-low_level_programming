ee0c5e9dd960982dfca0dbc038997e584d209486
Aphranm <aphranm@gmail.com>
2023-01-07 09:33:54 -0800

    o is upper

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,7 @@
+
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 int _putchar(char c);
 int _isupper(int c);
@@ -13,5 +15,6 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-#endif /* MAIN_H */
+
+#endif 
 
